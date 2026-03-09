a5586de742d4d47670b968a985355bb76d33801f
nimnusmik <kimsunmin0227@gmail.com>
2026-03-09 11:48:19 +0900

    feat: use env-based API URL for production

--- tm_mob/src/api/client.js
+++ tm_mob/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const BASE_URL = 'http://10.0.2.2:8000/api/v1';
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://10.0.2.2:8000/api/v1';
 
 const client = axios.create({
   baseURL: BASE_URL,
